07ec110d2acccd453fbd9638bd973daba7c9e371
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-04-29 17:06:30 +0000

    Complete with javadoc

--- src/main/java/Cow.java
+++ src/main/java/Cow.java
@@ -1,33 +1,62 @@
+/**
+ * stores information of a cow, can be compared to another cow
+ * @author Simon Shao
+ * @version 1.0.0
+ */
 public class Cow{
 
     private String name;
     private int age;
     private double weight;
 
+    /**
+     * initializes cow information
+     * @param name variable for name of cow
+     * @param age variable for cow age
+     * @param weight variable for weight of cow
+     */
     public Cow(String name, int age, double weight){
         this.name = name;
         this.age = age;
         this.weight = weight;
     }
-
+    /**
+     * to access name of cow
+     * @return returns the name of the cow
+     */
     public String getName(){
         return this.name;
     }
-
+/**
+     * to access age of cow
+     * @return returns the age of the cow
+     */
     public int getAge(){
         return this.age;
     }
 
+    /**
+     * to access weight of cow
+     * @return returns the weight of cow
+     */
     public double getWeight(){
         return this.weight;
     }
 
-
+/**
+     * overrides the toString method to print out all information of cow
+     * @return returns a string of all informations of cow
+     */
     @Override
     public String toString(){
         return this.name + ", " + this.age + " - " + this.weight;
     }
 
+    /**
+ * compares 2 cows to see if they are the same. 
+ * @param obj Another cow as an object that is to be compared with the current student
+ * @return returns whether they are the same cow or not
+ */
     @Override
     public boolean equals(Object obj){
         try{
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,33 +1,60 @@
+/**
+ * Stores information of a student, can be compared to another student
+ * @author Simon Shao
+ * @version 1.0.0
+ */
 public class Student{
 
     private String name;
     private int age;
     private String studentNumber;
-
+    /**
+     * initializes student information
+     * @param name variable for name of student
+     * @param age variable for student age
+     * @param studentNumber variable for student number (ID)
+     */
     public Student(String name, int age, String studentNumber){
         this.name = name;
         this.age = age;
         this.studentNumber = studentNumber;
     }
 
+    /**
+     * to access name of student
+     * @return returns the name of the student
+     */
     public String getName(){
         return this.name;
     }
-
+/**
+     * to access age of student
+     * @return returns the age of the student
+     */
     public int getAge(){
         return this.age;
     }
-
+/**
+     * to access ID of student
+     * @return returns the ID of the student
+     */
     public String getStudentNumber(){
         return this.studentNumber;
     }
 
-
+    /**
+     * overrides the toString method to print out all information of student
+     * @return returns a string of all informations of students
+     */
     @Override
     public String toString(){
         return this.name + ", " + this.age + " - " + this.studentNumber;
     }
-
+/**
+ * compares 2 students to see if they are the same person. 
+ * @param obj Another student as an object that is to be compared with the current student
+ * @return returns whether they are the same student or not
+ */
     @Override
     public boolean equals(Object obj){
         try{
